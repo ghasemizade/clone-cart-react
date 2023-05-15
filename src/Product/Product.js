4014b3488ae9064a552e3522edacf58e4d16f810
Hosein Ghasemizade <92257857+ghasemizade@users.noreply.github.com>
2023-05-15 14:59:10 +0330

    update style

---
+++ src/Product/Product.js
@@ -0,0 +1,19 @@
+import React from "react"
+import './Product.css'
+
+function Product() {
+    return(
+        <div>
+            {/* <h2>Product Card</h2> */}
+            <div className="card">
+                <img src='iphon13.jpg' alt="Product"/>
+                <h1>IPHONE</h1>
+                <p className="iphone-price">$999</p>
+                <p>iPhone 13 mini, the smallest member of the family, but powerful</p>
+                <button>Add to Card</button>
+            </div>
+        </div>
+    )
+}
+
+export default Product
