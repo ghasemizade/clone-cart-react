b283849189e0eafa59e017e7e1a5f7498179cdee
Hosein Ghasemizade <92257857+ghasemizade@users.noreply.github.com>
2023-05-18 16:36:59 +0330

    update props

--- src/App.js
+++ src/App.js
@@ -4,9 +4,9 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <Product/>
-      <Product/>
-      <Product/>
+      <Product title="iphone" image="/01.jpg" price="1,999"/>
+      <Product title="samsung" image="/02.jpg" price="1,499"/>
+      <Product title="xiaomi" image="/03.jpg" price="999"/>
     </div>
   );
 }
--- src/Product/Product.js
+++ src/Product/Product.js
@@ -1,14 +1,14 @@
 import React from "react"
 import './Product.css'
 
-function Product() {
+function Product(props) {
     return(
         <div className="container">
             {/* <h2>Product Card</h2> */}
             <div className="card">
-                <img src='iphon13.jpg' alt="Product"/>
-                <h1>IPHONE</h1>
-                <p className="price">$999</p>
+                <img src={props.image} alt="Product"/>
+                <h1>{props.title}</h1>
+                <p className="price">${props.price}</p>
                 <p className="about">iPhone 13 mini, the smallest member of the family, but powerful</p>
                 <button>Add to Card</button>
             </div>
