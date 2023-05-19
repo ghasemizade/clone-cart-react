c6c5ce7d17f21fc00117180e3bc7dd5653d47a27
Hosein Ghasemizade <92257857+ghasemizade@users.noreply.github.com>
2023-05-19 11:04:53 +0330

    update spread sytax

--- src/App.js
+++ src/App.js
@@ -2,11 +2,18 @@ import Product from './Product/Product';
 import './App.css';
 
 function App() {
+
+  const allProducts = [
+    {id: 1, title:"iphone", image:"01.jpg", price:"1,999"},
+    {id: 2, title:"samsung", image:"02.jpg", price:"1,499"},
+    {id: 3, title:"xiaomi", image:"03.jpg", price:"999"},
+  ]
+
   return (
     <div className="App">
-      <Product title="iphone" image="/01.jpg" price="1,999"/>
-      <Product title="samsung" image="/02.jpg" price="1,499"/>
-      <Product title="xiaomi" image="/03.jpg" price="999"/>
+      <Product {...allProducts[0]}/>
+      <Product {...allProducts[1]}/>
+      <Product {...allProducts[2]}/>
     </div>
   );
 }
--- src/Product/Product.js
+++ src/Product/Product.js
@@ -1,13 +1,13 @@
 import React from "react"
 import './Product.css'
 
-function Product(props) {
+function Product({image, title, price}) {
     return(
         <div className="container">
             <div className="card">
-                <img src={props.image} alt="Product"/>
-                <h1>{props.title}</h1>
-                <p className="price">${props.price}</p>
+                <img src={image} alt="Product"/>
+                <h1>{title}</h1>
+                <p className="price">${price}</p>
                 <p className="about">iPhone 13 mini, the smallest member of the family, but powerful</p>
                 <button>Add to Card</button>
             </div>
